09e213824ef409ddf00880f0fb004b23e74dfc19
Xan Nick <xan.nick@plyint.com>
2020-02-28 23:59:15 -0600

    Initial commit for nmcli-toggle-ipv6.sh

---
+++ README.md
@@ -0,0 +1,18 @@
+# nmcli-toggle-ipv6.sh
+
+nmcli-toggle-ipv6.sh wraps the NetworkManager CLI tool to allow simple toggling of IPv6 on a network interface.  Disabling IPv6 is purposefully designed to be temporary, so that rebooting or disabling/reenabling the network interface will restore IPv6 functionality.  
+
+It is intended to be used when IPv6 is suspected of causing a connectivity problem and needs to be temporarily disabled.  For example, it is known that some ISPs do not properly handle IPv6 routing especially on mobile networks.  (e.g. https://github.com/rubygems/rubygems/issues/2253)
+
+## Requirements
+
+nmcli-toggle-ipv6.sh requires that NetworkManager is installed on the Linux distribution.  (This is normally installed by default on Ubuntu)
+
+NetworkManager is a part of the [GNOME](https://www.gnome.org) desktop environment.
+
+## Usage
+
+Make sure the script has executable permissions and then invoke it from the command line.
+
+./nmcli-toggle.sh (No arguments, the current active wifi interface will be toggled for ipv6)
+./nmcli-toggle.sh <iface> (The specified interface will be toggled for ipv6)
---
+++ nmcli-toggle-ipv6.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+################################################################################
+# Copyright (c) 2020 Plyint, LLC <contact@plyint.com>. All Rights Reserved.
+# This file is licensed under the MIT License (MIT). 
+# Please see LICENSE.txt for more information.
+#
+# DESCRIPTION:
+# Turns ipv6 on/off for the network interface using NetworkManager.
+# Tested on Ubuntu.
+################################################################################
+
+# If a network interface is not specified,
+# then take the inteface from the current active wifi connection.
+if [ ! -z "$1" ]; then
+	IFACE="$1"
+else
+	IFACE="$(nmcli --fields type,device connection show --active | grep wifi | awk '{printf $2}')"
+fi
+
+# Running this command with sudo, so that we prompt for password at the start
+# rather than at the end when we might have to flush the ip addresses.
+NAME="$(sudo nmcli --fields name,device connection show --active | grep "$IFACE" | awk -F "  " '{printf $1}')"
+STATUS="$(nmcli --fields ipv6.method connection show --active "$NAME" | awk '{printf $2}')"
+echo "Toggling IPv6 for network interface \"$IFACE\""
+if [ "$STATUS" = "ignore" ]; then
+	echo "IPv6 currently disabled.  Enabling..."
+  nmcli connection modify "$NAME" ipv6.method "auto"
+	echo "NOTE: If you are using a mobile hotspot please turn it off/on now."
+	CONN_ACTIVATED="$(sudo nmcli connection up "$NAME" | grep success)"
+	if [ ! -z "$CONN_ACTIVATED" ]; then
+		echo "IPv6 enabled."
+	fi
+else
+	# Have to flush the IPs here as even cycling the connection
+	# would not always cause the IP6 Addresses to be removed.
+	echo "IPv6 currently enabled.  Disabling..."
+  nmcli connection modify "$NAME" ipv6.method "ignore"
+	sudo ip -6 addr flush "$IFACE"
+	echo "IPv6 disabled."
+fi
